a5b2851e01e5c39fd65337af8bb250b848b88ed2
aditya <es1221211@iitd.ac.in>
2024-06-25 15:54:39 +0530

    dependencies , registers and cond_f added

--- vm.c
+++ vm.c
@@ -1,4 +1,16 @@
 // Building the LC-3 VM
+// Dependencies
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+/* unix only */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 // Memory
 #define MEMORY_MAX (1 << 16)
@@ -21,4 +33,31 @@ enum {
 
 // Store registers in an array
 uint16_t reg[R_COUNT];
+
+// Conditional flags
+enum{
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2,
+};
+
+// Opcodes
+enum{
+    OP_BR = 0,
+    OP_ADD, // Add
+    OP_LD, // Load
+    OP_ST, // Store
+    OP_JSR, // Jump register
+    OP_AND, // Bitwise and
+    OP_LDR, // Load register
+    OP_STR, // Store register
+    OP_RTI, // Unused
+    OP_NOT, // Bitwise not
+    OP_LDI, // Load direct
+    OP_STI, // Store indirect
+    OP_JMP, // Jump
+    OP_RES, // Reserved(unused)
+    OP_LEA, // Load effective address
+    OP_TRAP // Execute Trap
+};
 
